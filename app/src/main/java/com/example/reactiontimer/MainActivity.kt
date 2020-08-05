764047fb877f25cc87574bcdf2c4b33e4e09775e
Andreas Nikolaou <andnik1998@gmail.com>
2020-08-05 11:45:40 +0100

    added icon and title in app

--- app/src/main/java/com/example/reactiontimer/MainActivity.kt
+++ app/src/main/java/com/example/reactiontimer/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.reactiontimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    enum class TimerState {
+        Stopped, Running
+    }
+
+    private lateinit var timer: Timer
+    private var timerMiliSeconds: Long = 0
+    // private var timerState: TimerState.Stopped
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setIcon(R.drawable.ic_timer)
+        supportActionBar?.title= "     Reaction Timer"
     }
 }
